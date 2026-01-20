b69ae60fe599a4f934f31ed457de8b7c2c26a203
ylmzfun <ylmzfun@163.com>
2026-01-20 17:04:16 +0800

    诗词

--- docs/.vuepress/.temp/git/config.js
+++
@@ -1,9 +0,0 @@
-import { GitContributors } from "/Users/ylmzfun/Documents/study/note/poetry/node_modules/@vuepress/plugin-git/lib/client/components/GitContributors.js";
-import { GitChangelog } from "/Users/ylmzfun/Documents/study/note/poetry/node_modules/@vuepress/plugin-git/lib/client/components/GitChangelog.js";
-
-export default {
-  enhance: ({ app }) => {
-    app.component("GitContributors", GitContributors);
-    app.component("GitChangelog", GitChangelog);
-  },
-};
